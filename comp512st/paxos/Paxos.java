5ac53f735850edd6c322ad27d2503aafccd29f80
alkbed <alek.bedard@mail.mcgill.ca>
2022-10-26 20:54:24 -0400

    PAXOS - Phase 1 + implementation of paxos internal

--- comp512st/paxos/Paxos.java
+++ comp512st/paxos/Paxos.java
@@ -7,6 +7,8 @@ import java.util.logging.*;
 import java.util.Dictionary;
 import java.util.Hashtable;
 import comp512.utils.*;	
+import java.util.LinkedList;
+import java.util.Queue;
 
 // Any other imports that you may need.	// Any other imports that you may need.
 import java.io.*;	
@@ -33,11 +35,14 @@ public class Paxos
 	private static final String PAXOS_PHASE_PROMISE_ACCEPT = "promiseaccept";
 	private static final String PAXOS_PHASE_PROMISE_DENY = "promisedeny";
 	private static final String PAXOS_PHASE_CONFIRM_VALUE = "confirmvalue";
+	private static final String PAXOS_PHASE_PROPOSE_VALUE_ACCEPTACK = "acceptack";
+	private static final String PAXOS_PHASE_PROPOSE_VALUE_DENYACK = "denyack";
 	
 	private int processCount;
 	private Dictionary<String, TriStateResponse> promises;
 	private String[] allProcesses;
-
+	private Queue<Object> messagesQueue;
+	private Thread paxosThread;
 	public Paxos(String myProcess, String[] allGroupProcesses, Logger logger, FailCheck failCheck) throws IOException, UnknownHostException
 	{
 		// Rember to call the failCheck.checkFailure(..) with appropriate arguments throughout your Paxos code to force fail points if necessary.
@@ -48,7 +53,11 @@ public class Paxos
 
 		processCount = allGroupProcesses.length;
 		allProcesses = allGroupProcesses;
+		messagesQueue = new LinkedList<Object>();
 		resetPromises();
+
+		paxosThread = new Thread(new PaxosThread());
+		paxosThread.start();
 	}
 
 	synchronized private void resetPromises()
@@ -63,10 +72,6 @@ public class Paxos
 	// This is what the application layer is going to call to send a message/value, such as the player and the move
 	public void broadcastTOMsg(Object val)
 	{
-		Object[] obj = (Object[])val;
-		int playerNum = (Integer)obj[0];
-		char move = (Character)obj[1];
-
 		//First step: Propose to be the leader
 		while (!propose())
 		{
@@ -79,62 +84,76 @@ public class Paxos
 		}
 		resetPromises();
 
-		Object[] returnObj = new Object[] { PAXOS_PHASE_CONFIRM_VALUE, val };
-		gcl.broadcastMsg(returnObj);
+		//Second step: Propose a value
+		//TODO
+
+		//Third step confirm value
+		confirmValue(val);
+	}
+
+	private void handleProposeLeaderMessage(String senderProcess, Object proposalID)
+	{
+		double proposerBallotID = (double)proposalID;
+		if (proposerBallotID > currentBallotID)
+		{
+			currentBallotID = proposerBallotID;
+			Object[] acceptMsg = new Object[] { PAXOS_PHASE_PROMISE_ACCEPT, currentBallotID };
+			gcl.sendMsg(acceptMsg, senderProcess);
+		}
+		else
+		{
+			Object[] denyMsg = new Object[] { PAXOS_PHASE_PROMISE_DENY, currentBallotID };
+			gcl.sendMsg(denyMsg, senderProcess);
+		}
+	}
+
+	private void handlePromiseAcceptMessage(String senderProcess)
+	{
+		synchronized(promises)
+		{
+			try
+			{
+				promises.remove(senderProcess);
+				promises.put(senderProcess, TriStateResponse.ACCEPT);
+			}
+			catch (Exception e)
+			{}
+		}
+	}
+
+	private void handlePromiseDenyMessage(String senderProcess)
+	{
+		synchronized(promises)
+		{
+			try
+			{
+				promises.remove(senderProcess);
+				promises.put(senderProcess, TriStateResponse.DENY);
+			}
+			catch (Exception e){}
+		}
+	}
+
+	private void handleConfirmValue(Object message)
+	{
+		messagesQueue.add(message);
 	}
 
 	// This is what the application layer is calling to figure out what is the next message in the total order.
 	// Messages delivered in ALL the processes in the group should deliver this in the same order.
 	public Object acceptTOMsg() throws InterruptedException
 	{
-		GCMessage gcmsg = gcl.readGCMessage();
-		
-		Object[] obj = (Object[])gcmsg.val;
-
-		if (obj[0] == PAXOS_PHASE_PROPOSE_LEADER)
+		Object message = messagesQueue.peek();
+		while (message == null)
 		{
-			double proposerBallotID = (double)obj[1];
-			if (proposerBallotID > currentBallotID)
+			try 
 			{
-				currentBallotID = proposerBallotID;
-				Object[] acceptMsg = new Object[] { PAXOS_PHASE_PROMISE_ACCEPT, currentBallotID };
-				gcl.sendMsg(acceptMsg, gcmsg.senderProcess);
+				Thread.sleep(THREAD_SLEEP_MILLIS);
+				message = messagesQueue.peek();
 			}
-			else
-			{
-				Object[] denyMsg = new Object[] { PAXOS_PHASE_PROMISE_DENY, currentBallotID };
-				gcl.sendMsg(denyMsg, gcmsg.senderProcess);
-			}
+			catch (Exception e) {}
 		}
-		else if (obj[0] == PAXOS_PHASE_PROMISE_ACCEPT)
-		{
-			synchronized(promises)
-			{
-				try
-				{
-					promises.remove(gcmsg.senderProcess);
-					promises.put(gcmsg.senderProcess, TriStateResponse.ACCEPT);
-				}
-				catch (Exception e)
-				{}
-			}
-		}
-		else if (obj[0] == PAXOS_PHASE_PROMISE_DENY)
-		{
-			synchronized(promises)
-			{
-				try
-				{
-					promises.remove(gcmsg.senderProcess);
-					promises.put(gcmsg.senderProcess, TriStateResponse.DENY);
-				}
-				catch (Exception e){}
-			}
-		}
-		else if (obj[0] == PAXOS_PHASE_CONFIRM_VALUE)
-			return obj[1];
-
-		return gcmsg.val;
+		return messagesQueue.remove();
 	}
 
 	// Add any of your own shutdown code into this method.
@@ -162,6 +181,12 @@ public class Paxos
 		return response == TriStateResponse.ACCEPT;
 	}
 
+	private void confirmValue(Object val)
+	{
+		Object[] obj = new Object[] {PAXOS_PHASE_CONFIRM_VALUE, val};
+		gcl.broadcastMsg(obj);
+	}
+
 	private TriStateResponse hasMajority()
 	{
 		int acceptCount = 0;
@@ -176,13 +201,13 @@ public class Paxos
 			else if (promises.get(process) == TriStateResponse.ACCEPT)
 			{
 				acceptCount ++;
-				if (((double)acceptCount / (double)processCount) > 0.5)
+				if (((double)acceptCount / (double)(processCount - 1)) > 0.5)
 					return TriStateResponse.ACCEPT;
 			}
 			else if (promises.get(process) == TriStateResponse.DENY)
 			{
 				denyCount ++;
-				if (((double)denyCount / (double)processCount) > 0.5)
+				if (((double)denyCount / (double)(processCount - 1)) > 0.5)
 					return TriStateResponse.DENY;
 			}
 		}
@@ -192,6 +217,51 @@ public class Paxos
 	private enum TriStateResponse
 	{
 		NORESPONSE, ACCEPT, DENY
+	}
+
+	private class PaxosThread implements Runnable
+	{
+		public void run()
+		{
+			try
+			{
+				GCMessage gcmsg = gcl.readGCMessage();
+				Object[] obj = (Object[])gcmsg.val;
+
+				if (obj[0].equals(PAXOS_PHASE_PROPOSE_LEADER))
+				{
+					handleProposeLeaderMessage(gcmsg.senderProcess, obj[1]);
+				}
+				else if (obj[0].equals(PAXOS_PHASE_PROMISE_ACCEPT))
+				{
+					handlePromiseAcceptMessage(gcmsg.senderProcess);
+				}
+				else if (obj[0].equals(PAXOS_PHASE_PROMISE_DENY))
+				{
+					handlePromiseDenyMessage(gcmsg.senderProcess);
+				}
+				else if (obj[0].equals(PAXOS_PHASE_PROPOSE_VALUE))
+				{
+					//TODO: Phase 2.
+				}
+				else if (obj[0].equals(PAXOS_PHASE_PROPOSE_VALUE_ACCEPTACK))
+				{
+					//TODO: Phase 2.
+				}
+				else if (obj[0].equals(PAXOS_PHASE_PROPOSE_VALUE_DENYACK))
+				{
+					//TODO: Phase 2.
+				}
+				else if (obj[0].equals(PAXOS_PHASE_CONFIRM_VALUE))
+				{
+					handleConfirmValue(obj[1]);
+				}
+			}
+			catch (InterruptedException ie) 
+			{ 
+				
+			}
+		}
 	}
 }
 
